03e9bdba01ace7f12c7fc9a1ad6a13241605f2af
Divyasree <divyasree.bona2005@gmail.com>
2026-06-09 00:44:46 +0530

    add login and logout controller

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { ApiError } from "../utility/ApiError"
+import jwt from "jsonwebtoken"
+import {User} from "../models/user.model.js"
+
+const verifyJWT = asyncHandler((req, res, next) => {
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+  try{
+    if(!token){
+      throw new ApiError(401, "Unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    if(!user){
+      throw new ApiError(401, "Invalid token")
+    }
+
+    req.user = user
+    next()
+  } catch(error){
+    throw new ApiError(401, error?.message || "Invalid access token")
+  }
+})
+
+export {verifyJWT}
